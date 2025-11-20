fdf56f548e3145625ba6bc537f939e733b913230
maswezzyyy <satrio.budiharto03@gmail.com>
2025-11-20 13:58:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,30 @@ if (toggleButton) {
     toggleButton.classList.toggle("active");
   });
 }
+// =========================
+// DARK MODE TOGGLE
+// =========================
+
+const toggleBtn = document.querySelector(".theme-toggle");
+const body = document.body;
+
+// Load theme sebelumnya
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+}
+
+// Toggle dark mode saat ikon diklik
+toggleBtn.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  // Simpan preferensi user
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
+});
+
 <<<<<<< HEAD
 
 =======
